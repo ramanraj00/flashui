033a04ffdb182513b7eefbaea74738b71d7c6944
ramanraj00 <r02519625@gmail.com>
2026-01-10 20:57:26 +0530

    initial setup of torch component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const flash = () => {
+
+
+  return <div className= "bg-gray-300 min-h-screen flex justify-center items-center">
+
+   <div className="h-80 w-64 bg-black border border-gray-600  rounded-[65px] overflow-hidden flex justify-center items-end">
+      <img className="h-30 w-25 mb-4 " src="/torch.png" alt="" />
+   </div>
+
+   
+  </div>
+
+}
+
+export default flash
